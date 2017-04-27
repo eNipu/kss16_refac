aa1fb6032a1a35ec37c82d3931b89bb2837155aa
Md Al-Amin Khandaker <xion.nipu@gmail.com>
2017-04-27 10:41:11 +0900

    Optimal Miller OK

--- KSS16/Line_evaluation.h
+++ KSS16/Line_evaluation.h
@@ -9,6 +9,13 @@
 #ifndef Line_evaluation_h
 #define Line_evaluation_h
 
-#include <stdio.h>
+#include "EFp16.h"
+
+extern void ltt_q(struct Fp16 *ANS,struct EFp16 *T,struct EFp16 *Q);
+extern void v2t_q(struct Fp16 *ANS,struct EFp16 *T,struct EFp16 *Q);
+extern void ltp_q(struct Fp16 *ANS,struct EFp16 *T,struct EFp16 *P,struct EFp16 *Q);
+extern void vtp_q(struct Fp16 *ANS,struct EFp16 *T,struct EFp16 *Q);
+extern void ADD_LINE(struct Fp16 *l_ANS,struct EFp16 *T_ANS,struct EFp16 *T,struct EFp16 *P,struct EFp16 *Q,struct Fp16 *Qx_neg);
+extern void DBL_LINE(struct Fp16 *l_ANS,struct EFp16 *T_ANS,struct EFp16 *T,struct EFp16 *Q,struct Fp16 *Qx_neg);
 
 #endif /* Line_evaluation_h */
---
+++ KSS16/Optimal_Millers_Algo.h
@@ -0,0 +1,14 @@
+//
+//  Optimal_Millers_Algo.h
+//  KSS16
+//
+//  Created by Khandaker Md. Al-Amin on 4/27/17.
+//  Copyright © 2017 Khandaker Md. Al-Amin. All rights reserved.
+//
+
+#ifndef Optimal_Millers_Algo_h
+#define Optimal_Millers_Algo_h
+
+#include "Line_evaluation.h"
+extern void Optimal_Miller(struct Fp16 *ANS,struct EFp16 *P,struct EFp16 *Q,mpz_t roop);
+#endif /* Optimal_Millers_Algo_h */
