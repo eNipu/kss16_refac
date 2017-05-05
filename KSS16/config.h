1b94de65175aab82fb1aca2b8f7945919127da4e
Md Al-Amin Khandaker <xion.nipu@gmail.com>
2017-05-05 18:23:56 +0900

    Commnet added

--- KSS16/config.h
+++ KSS16/config.h
@@ -1,17 +1,48 @@
-//
-//  config.h
-//  KSS16
-//
-//  Created by Khandaker Md. Al-Amin on 4/26/17.
-//  Copyright © 2017 Khandaker Md. Al-Amin. All rights reserved.
-//
+/*
+ * ELiPS is an Efficient Library for Pairing-based Systems
+ * Copyright (C) 2008-2017 ELiPS Authors. Please refer to the COPYRIGHT file
+ * for contact information.
+ *
+ * This file is part of ELiPS Project. ELiPS is legal property of its
+ * developers, whose names are not listed here.
+ *
+ * ELiPS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * ELiPS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ELiPS. If not, see <http://www.gnu.org/licenses/>.
+ */
 
+
+/** @file config.h
+ *  @brief Brief description about the source file/header file.
+ *
+ *  Then here write about the small details what this source file is intended for.
+ *  If it calculated prime field arithmetic then write what are the major funcion
+ *  ality it provides though the public functions.  For example what type of
+ *  public funtions it will give or global variables you will need.
+ *
+ *  @author Khandaker Md. Al-Amin(khandaker@s.okayama-u.ac.jp)
+ *  @date 4/26/17
+ *  @bug No know bug yet
+ */
 
 #ifndef config_h
 #define config_h
 
-#include <stdio.h>
-#include <gmp.h>
+/* -- Includes -- */
+/* libc inlcudes. */
+#include <stdio.h> /* for standard input out */
+
+/* libgmp inlcudes. */
+#include <gmp.h> /* for big integer operation*/
 
 
 #endif /* config_h */
